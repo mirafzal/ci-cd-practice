9fdd06e4aa27ba8a4378d3999cc43cd62efeeea1
= <shavkatovm99@gmail.com>
2025-09-24 21:11:42 +0500

    add some params for celery

--- mydjangoapp/mydjangoapp/celery.py
+++ mydjangoapp/mydjangoapp/celery.py
@@ -18,6 +18,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
+app.conf.task_acks_late = True
+app.conf.worker_prefetch_multiplier = 1
+app.conf.broker_transport_options = {"visibility_timeout": 3600}
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
